d5039c223751bee093d88c9e665ae22431a29365
GammaGVA <gri_v_a@vk.com>
2023-01-11 14:22:42 +0300

    Version 0.1 всё впереди. Сделал чуть читабельнее, улучшил ифки.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from time import time
 from random import randrange
 
 
-def sortted(spisok: list):
+def clone_sorted(spisok: list):
     '''Реализация быстрой сортировки.
     Проверял скорость работы выполненой сортировки в пайтон,
-    для будущей сверки по времени и понимая полезности следующей сортировки.'''
+    для будущей сверки по времени уже со своей сортировкой.'''
     if len(spisok) > 1:
         centr = len(spisok) // 2
         elem = [spisok.pop(centr)]
@@ -19,27 +19,27 @@ def sortted(spisok: list):
             else:
                 elem.append(i)
 
-        return sortted(men) + elem + sortted(big)
+        return clone_sorted(men) + elem + clone_sorted(big)
     else:
         return spisok
 
 
 def sortirovka(spisok: list):
-    '''Сортировка по налогии с бинарным поиском.
+    '''Сортировка по аналогии с бинарным поиском.
     Берём каждый элемент списка(строки\кортежа) и вставляем в новый по принципу бинарного поиска.
     Главная особенность, перечень должен состоять из однотипных эелементов, все числа, или все строки\буквы.'''
     if len(spisok) > 1:
         # Это иф не смог обойти, что если список будет состоять менее чем из 2 элементов.
         elem1 = spisok[0]
         elem2 = spisok[1]
-        spisok = tuple(spisok[2:])
         n_spisok = [min(elem1, elem2), max(elem1, elem2)]
 
-        for elem in spisok:
+        for elem in spisok[2:]:
             centr_index = len(n_spisok) // 2
             l_index = 0
             r_index = len(n_spisok) - 1
             # Узнаём крайнии индексы нового списка и центральный индекс.
+
             while True:
                 # не стоит тут боятся вайл тру, так как цикл только при сбои может дойти до 1000 повторений.
                 # 2**32 = 4 294 967 296 - это примерно столько элементов в новом списке при стольких то повторений.
@@ -54,29 +54,18 @@ def sortirovka(spisok: list):
                     centr_index = (r_index + l_index) // 2
                     # коректируем область в которой ищем индекс.
 
-                elif elem == n_spisok[centr_index]:
-                    n_spisok.insert(centr_index, elem)
-                    break
-                    # Вставляем элемент и закрываем цикл так как он вечный
-
-                if r_index == l_index and elem < n_spisok[l_index]:
+                if (r_index - l_index) in (1, 0) and elem <= n_spisok[l_index]:
                     n_spisok.insert(l_index, elem)
                     break
 
-                elif r_index == l_index and elem > n_spisok[r_index]:
+                elif (r_index - l_index) in (1, 0) and elem >= n_spisok[r_index]:
                     n_spisok.insert(r_index + 1, elem)
                     break
 
-                elif r_index - l_index == 1 and n_spisok[l_index] < elem < n_spisok[r_index]:
-                    n_spisok.insert(r_index, elem)
-                    break
-
-                elif r_index - l_index == 1 and (elem < n_spisok[l_index] or elem == n_spisok[l_index]):
-                    n_spisok.insert(l_index, elem)
-                    break
-
-                elif r_index - l_index == 1 and (elem > n_spisok[r_index] or elem == n_spisok[r_index]):
-                    n_spisok.insert(r_index + 1, elem)
+                elif r_index - l_index == 1 and n_spisok[l_index] < elem < n_spisok[r_index] or elem == n_spisok[
+                    centr_index]:
+                    # В ситуации r_index - l_index == 1 l_index всегда равен centr_index (проверил на практике).
+                    n_spisok.insert(centr_index + 1, elem)
                     break
 
         return n_spisok
@@ -85,96 +74,59 @@ def sortirovka(spisok: list):
         return tuple(spisok)
 
 
-LS = []
+def statistics(len_list=100000, max_elem=100000):
+    LS = []
 
-for i in range(100000):
-    # Формирую рандомный список
-    LS.append(randrange(0, 100000))
+    for i in range(len_list):
+        # Формирую рандомный список
+        LS.append(randrange(0, max_elem))
 
-start = time()
-_a = sorted(LS)
-time_sorted = (time() - start)
-print(time_sorted)
+    start = time()
+    _sorted = sorted(LS)
+    time_sorted = (time() - start)
 
-start = time()
-a = sortirovka(LS)
-time_sortirovka = (time() - start)
-print(time_sortirovka)
+    start = time()
+    _sortirovka = sortirovka(LS)
+    time_sortirovka = (time() - start)
 
-start = time()
-c = sortted(LS)
-time_sortted = (time() - start)
-print(time_sortted)
+    start = time()
+    _clone_sorted = clone_sorted(LS)
+    time_clone_sorted = (time() - start)
 
-print(f'sorted быстрее sortirovka в {time_sortirovka / time_sorted}')
-print(f'sorted быстрее sortted в {time_sortted / time_sorted}')
-print(f'sortted быстрее sortirovka в {time_sortirovka / time_sortted}')
+    with open(f'statistics{len_list}.txt', 'a') as file:
+        file.write(f'''\nДлинна списка {len_list}
+Элемент принимает значение от 0 до {max_elem}.
+sorted выполнил сортровку за {time_sorted}
+sortirovka выполнил сортровку за {time_sortirovka}
+clone_sorted выполнил сортровку за {time_clone_sorted}
+sorted быстрее sortirovka в {time_sortirovka / time_sorted}
+sorted быстрее clone_sorted в {time_clone_sorted / time_sorted}
+clone_sorted быстрее sortirovka в {time_sortirovka / time_clone_sorted}\n''')
+        file.write('_' * 20)
 
-for i, ii in zip(a, c):
-    if i != ii:
-        print('Лох')
-else:
-    print("Красавчик")
 
-'''
-Это я ещё сравнивал разницу скорости у себя на версиях пайтон 3.10 и 3.11.
+LS = [10, 100, 1000, 10000, 100000, 200000, 300000, 400000, 500000]
+LE = [10, 100, 1000, 10000, 100000]
 
-На 3.10
-0.030642032623291016
-109.65123796463013
-0.4462869167327881
-sorted быстрее sortirovka в 3578.458365104807
-sorted быстрее sortted в 14.564533698510761
-sortted быстрее sortirovka в 245.69673421612586
-_______________________________________________
-0.029674768447875977
-113.8092451095581
-0.4175376892089844
-sorted быстрее sortirovka в 3835.2193146667737
-sorted быстрее sortted в 14.070461575543325
-sortted быстрее sortirovka в 272.5723881960623
-_______________________________________________
+for i in LS:
+    for j in LE:
+        for _ in range(3):
+            statistics(len_list=i, max_elem=j)
+        with open(f'statistics{i}.txt', 'a') as file:
+            file.write('\nСледующая размерность=========================!\n')
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-На 3.11
-
-0.02505660057067871
-102.63605499267578
-0.21150755882263184
-sorted быстрее sortirovka в 4096.168380988629
-sorted быстрее sortted в 8.441191303106713
-sortted быстрее sortirovka в 485.2595130122294
-______________________________________________
-0.02314019203186035
-100.64344453811646
-0.2205972671508789
-sorted быстрее sortirovka в 4349.2916739647835
-sorted быстрее sortted в 9.533078500262732
-sortted быстрее sortirovka в 456.23160176903156
-______________________________________________
-0.02330470085144043
-111.60277915000916
-0.23963069915771484
-sorted быстрее sortirovka в 4788.852680900693
-sorted быстрее sortted в 10.282504833907947
-sortted быстрее sortirovka в 465.7282207258299
-______________________________________________
-
-После того как дурочок убрал конкатенацию кортежа и просто вставляет в список по индексу.
-
-0.022422075271606445
-1.6129510402679443
-0.20899462699890137
-sorted быстрее sortirovka в 71.93584985911001
-sorted быстрее sortted в 9.320931468977617
-sortted быстрее sortirovka в 7.717667499061702
-______________________________________________
-0.024276018142700195
-1.645150899887085
-0.2215421199798584
-sorted быстрее sortirovka в 67.76856444151993
-sorted быстрее sortted в 9.125966156293888
-sortted быстрее sortirovka в 7.425905737638759
-'''
-print('''Главный вывод, что это работает и не надо усложнять. Позже проверю на ещё большей длинне рандомного списка.
-Может что ещё с ифами придумаю, уже есть пару мыслишек.''')
+'''У меня xeon3440 и опереатива частотой 1600. Будь всё пошустрее время было бы пошустрее.
+Вывод: при увелечение списка на порядок при размере списка до 100.000 включительно, скорость уменьшалась на порядок,
+порой на порядок с коэфицентом.
+Принцип замедления скорости у всех одинаков, до 100.000 у всех падает скорость на порядок.
+200.000-400.000 увеличивается в 2-3 раза(если округлить).
+если сравнивать 400 и 500 тысяч то замедление произошло на 1/3 у всех эта зависимость(повторюсь) одинаковая.
+sortirovka при увеличение разброса велечины значения элемента скорость теряла но не значительно. Если сравнивать
+в пропорциях но меньше всего, даже почти без изменений.
+Вывод сделать не могу, данных мало, желательно бы запустить более дитальную проверку, начиная от 100.000 увеличивать
+на 10.000, также проводить тест не 3 раза а 1000. Удобнее будет смотреть в графике, это сделать можно но я не умею пока.
+Проверку выполнить хотя бы до 10_000_000. Возможности моего пк будут это делать год. Жопой чую что в Другом языке даст
+результат гораздо лучше, но на сколько не понимаю.
+Обнадёживает закономерность падения скорости, она у всех одинаковая при увелечение размерности списка,
+а при увелечение размерности элемента и вовсе почти не замедляется по сравнению с другими.
+Ещё бы попробовать реализовать это в Си.'''
